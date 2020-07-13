0bdb91c0feb377e2aa127a0cfb2ec7266e6066e9
kangs <kangs.passoubady@socotra.com>
2020-07-12 21:30:16 -0700

    added quiz example

--- src/test/java/com/kavinschool/osticket/pages/SupportCenterHomePage.java
+++ src/test/java/com/kavinschool/osticket/pages/SupportCenterHomePage.java
@@ -1,8 +1,6 @@
 package com.kavinschool.osticket.pages;
 
-import com.kavinschool.osticket.utils.DriverUtils;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
@@ -11,41 +9,26 @@ import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
 public class SupportCenterHomePage {
-	private WebDriver driver;
+    private WebDriver driver;
 
-	@FindBy(how = How.CSS, using = "input.button2 ")
-	//@CacheLookup
-	private WebElement openNewTicketButton;
+    @FindBy(how = How.CSS, using = "input.button2 ")
+    @CacheLookup
+    private WebElement openNewTicketButton;
 
-	public SupportCenterHomePage(WebDriver driver) {
-		this.driver = driver;
-	}
+    public SupportCenterHomePage(WebDriver driver) {
+        this.driver = driver;
+    }
 
-	public String getTitle() {
-		return driver.getTitle();
-	}
+    public String getTitle() {
+        return driver.getTitle();
+    }
 
-	public void clickAndOpenNewTicket() {
-		openNewTicketButton.click();
-	}
+    public void clickAndOpenNewTicket() {
+        openNewTicketButton.click();
+    }
 
-	public OpenNewTicketPage openNewTicket() {
-		try {
-			//DriverUtils.reduceImplicitWait(driver,5);
-			DriverUtils.delay(500);
-			// click methods are not working properly in IE11
-			//openNewTicketButton.click();
-            //Work around directly submit it
-            //DriverUtils.jsClick(openNewTicketButton,driver);
-            openNewTicketButton.sendKeys(Keys.ENTER);
-			//openNewTicketButton.submit();
-		}
-		catch (NoSuchElementException exp) {
-            exp.printStackTrace();
-        //} catch (InterruptedException exp) {
-        //    exp.printStackTrace();
-        }
-        //DriverUtils.reduceImplicitWait(driver,30);
-		return PageFactory.initElements(driver, OpenNewTicketPage.class);
-	}
+    public OpenNewTicketPage openNewTicket() {
+        openNewTicketButton.sendKeys(Keys.ENTER);
+        return PageFactory.initElements(driver, OpenNewTicketPage.class);
+    }
 }
